5ed7049a828805f91d5af2f418c4cb94452c9a67
saurabh2602pandey <87798736+saurabh2602pandey@users.noreply.github.com>
2021-12-10 15:08:40 +0530

    Update randompassword.go

--- randompassword.go
+++ randompassword.go
@@ -1,21 +1,54 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"math/rand"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+func dbConn() (db *sql.DB) {
+	dbDriver := "mysql"
+	dbUser := "newuser"
+	dbPass := "password"
+	dbName := "locker"
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+func generate() string {
 	rand.Seed(time.Now().UnixNano())
-	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
-	b := make([]rune, 25)
+	var letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+	b := make([]byte, 25)
+
 	for i := range b {
 		b[i] = letters[rand.Intn(len(letters))]
 
 	}
 	newpassword := string(b)
+
 	fmt.Println("password", newpassword)
 	fmt.Println(b)
+	db := dbConn()
+	sql := fmt.Sprintf("select user_name from entry where user_name= ('%s')", newpassword)
+	fmt.Println(sql)
+	_, err := db.Exec(sql)
+
+	if err != nil {
+		fmt.Println(err.Error())
+
+	}
+
+	defer db.Close()
+	return newpassword
+}
+func main() {
+	here := generate()
+	fmt.Println(here)
 
 }
